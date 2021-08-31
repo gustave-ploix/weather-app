3452df0ae2cc9dd52bfb842123b5a39dc59de3e9
gustave <gustaveploix.pro@gmail.com>
2021-08-31 17:10:43 +0200

    modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+import CitySelect from './pages/CitySelect';
+import Weather from './pages/Weather';
 
 export default function App (){
 
+    const [zip, setZip ] = useState("")
+
+    useEffect(() => {
+        console.log(zip);
+    }, [zip])
+
     return(
 
-        <div>
-            Hello World
-        </div>
+        <Router>
+             <main>
+                 <Switch>
+                     <Route exact path="/">
+                        <CitySelect zip={zip} setZip={setZip}/>
+                     </Route>
+                     <Route path="/weather/:zipCode">
+                        <Weather />
+                     </Route>
+                 </Switch>
+             </main>
+        </Router>
 
     )
 
